54e1dbe3521456a6df09118fbeac7bc2ce9354d7
dosmaster4life <jefferysuperman@gmail.com>
2022-09-17 17:50:28 -0600

    routes for localhost, and more.

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./database/connect');
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const app = express();
+
 require("dotenv").config();
 
 app.use(bodyParser.json()).use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-}).use('/',require('./routes'));
+}).use('/',require('./routes/index'));
 
-mongodb.initDb((err,mongodb) => {
-    if(err) {
-        console.log(err);
+
+mongodb.initDatabase((error,mongodb) => {
+    if(error) {
+        console.log(error);
     }else {
         app.listen(port) 
             console.log('Server started on port: ' + port);
